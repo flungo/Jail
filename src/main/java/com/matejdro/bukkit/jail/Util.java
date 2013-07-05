8ddf6ee3cb3b420e8738bf180c1100fe8681bc07
Luke <luke@lukeparker.co.uk>
2013-07-05 14:43:09 +0100

    Removed Uneeded code from Utill class

--- src/main/java/com/matejdro/bukkit/jail/Util.java
+++ src/main/java/com/matejdro/bukkit/jail/Util.java
@@ -27,33 +27,7 @@ public class Util {
     
 	public static void Message(String message, Player player)
 	{
-		message = message.replaceAll("\\&([0-9abcdef])", "�$1");
-		
-		String color = "f";
-		final int maxLength = 61; //Max length of chat text message
-        final String newLine = "[NEWLINE]";
-        ArrayList<String> chat = new ArrayList<String>();
-        chat.add(0, "");
-        String[] words = message.split(" ");
-        int lineNumber = 0;
-        for (int i = 0; i < words.length; i++) {
-                if (chat.get(lineNumber).length() + words[i].length() < maxLength && !words[i].equals(newLine)) {
-                        chat.set(lineNumber, chat.get(lineNumber) + (chat.get(lineNumber).length() > 0 ? " " : "&" + color ) + words[i]);
-
-                        if (words[i].contains("&")) color = Character.toString(words[i].charAt(words[i].indexOf("&") + 1));
-                }
-                else {
-                        lineNumber++;
-                        if (!words[i].equals(newLine)) {
-                                chat.add(lineNumber,  "&" + color + words[i]);
-                        }
-                        else
-                                chat.add(lineNumber, "");
-                }
-        }
-        for (int i = 0; i < chat.size(); i++) {
-                player.sendMessage(chat.get(i));
-        }
+		player.sendMessage(message);
 	}
 	
 	public static void Message(String message, CommandSender sender)
