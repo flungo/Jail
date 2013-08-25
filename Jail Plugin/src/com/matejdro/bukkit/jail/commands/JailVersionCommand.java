8184ff4f0c5ca3bfebf93a7d18ddc8a87270d15d
Luke <lukepipod@gmail.com>
2013-08-25 09:13:41 +0100

    Jail v2.5.6

--- Jail Plugin/src/com/matejdro/bukkit/jail/commands/JailVersionCommand.java
+++ Jail Plugin/src/com/matejdro/bukkit/jail/commands/JailVersionCommand.java
@@ -6,6 +6,12 @@ import com.matejdro.bukkit.jail.Jail;
 
 public class JailVersionCommand extends BaseCommand{
 	
+	public JailVersionCommand(){
+		adminCommand = false;
+		needPlayer = false;
+		permission = "jail.command.jailversion";
+	}
+	
 	@Override
 	public Boolean run(CommandSender sender, String[] args) {
 		sender.sendMessage("Jail Version: " + Jail.instance.getDescription().getVersion());
